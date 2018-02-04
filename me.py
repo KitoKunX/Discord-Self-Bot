1068614ca650a32cf4626e9e2c50711f476f02a2
Kito <KitoKunX@users.noreply.github.com>
2018-02-04 16:11:24 +1100

    Minor update, extra commands.
    Slightly optimized, remotely close bot with command, eval command. (unchecked for errors)

--- me.py
+++ me.py
@@ -13,7 +13,7 @@ me = discord.Client()
 
 #set prefix here
 prefix = "k#"
-
+    
 #handle message events here
 @me.event
 async def on_message(message):
@@ -32,14 +32,42 @@ async def on_message(message):
             command, args = msg[0], ""
         command = command[len(prefix):]
 
+        #delete your command usage message,
+        #remove this line if not needed
+        #or place in specific commands, etc.
+        await me.delete_message(message)
+
         #commands here
         if command == "embed":
-            await me.delete_message(message)
             fancy = discord.Embed(title = args, description = '', colour=0xDEADBF)
             fancy.set_author(name = 'Kito', icon_url = me.user.avatar_url)
             fancy.set_thumbnail(url = "https://cdn.discordapp.com/emojis/403695960347246593.png")
             fancy.set_footer(text="My dick hurts", icon_url=discord.Embed.Empty)
             await me.send_message(message.channel, embed = fancy)
+
+        #eval snippets
+        if command == "eval":
+            try:
+                ret = eval(args)
+                res = str(ret)
+            except Exception as e:
+                res = str(e)
+            fancy = discord.Embed(title = "Eval Result for " + args, description = res, colour=0xDEADBF)
+            fancy.set_author(name = 'Kito', icon_url = me.user.avatar_url)
+            fancy.set_thumbnail(url = "https://cdn.discordapp.com/emojis/403695960347246593.png")
+            fancy.set_footer(text="My dick hurts", icon_url=discord.Embed.Empty)
+            await me.send_message(message.channel, embed = fancy)
+
+        #safely close bot
+        if command == "leave":
+            try:
+                #can remove line below if you don't want custom message
+                await me.send_message(message.channel, "CUSTOM LEAVE MESSAGE HERE")
+                await me.logout()
+                await me.close()
+                sys.exit()
+            except Exceptions as e:
+                print("Something f***ed up: " + str(e))
 
 #Start the bot
 me.run("LOCAL STORAGE TOKEN GOES HERE", bot = False)
